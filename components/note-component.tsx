97d23941d8dc27afd5af20b9093b9488ec4de823
KeshavSoni17 <sonikeshav1607@gmail.com>
2025-06-07 10:00:07 +0530

    scroll

--- components/note-component.tsx
+++ components/note-component.tsx
@@ -153,7 +153,7 @@ export default function NoteComponent() {
   };
 
   return (
-    <SidebarInset className="overflow-auto h-full max-h-[100vh] [-webkit-overflow-scrolling:touch]">
+    <SidebarInset>
       <header className="flex h-14 shrink-0 items-center gap-2">
         <div className="flex flex-1 items-center gap-2 px-3">
           <SidebarTrigger />
--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -233,7 +233,7 @@ function SidebarRail({ className, ...props }: React.ComponentProps<"button">) {
 }
 
 function SidebarInset({ className, ...props }: React.ComponentProps<"main">) {
-  return <main data-slot="sidebar-inset" className={cn("bg-background relative flex w-full flex-1 flex-col", "md:peer-data-[variant=inset]:m-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow-sm md:peer-data-[variant=inset]:peer-data-[state=collapsed]:ml-2", className)} {...props} />;
+  return <main data-slot="sidebar-inset" className={cn("bg-background relative flex w-full flex-1 flex-col overflow-auto h-full max-h-[100vh] [-webkit-overflow-scrolling:touch]", "md:peer-data-[variant=inset]:m-2 md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow-sm md:peer-data-[variant=inset]:peer-data-[state=collapsed]:ml-2", className)} {...props} />;
 }
 
 function SidebarInput({ className, ...props }: React.ComponentProps<typeof Input>) {
